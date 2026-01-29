bd35f96f9e9748b6cefd6abfedd1336dd3bf59e5
kristjan arnar <kristjanarnar2009@gmail.com>
2026-01-29 10:19:48 +0000

    29.jan

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
 
 
-app.set('views', path.join(__dirname, 'src', 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
-
-
 app.use(express.static(path.join(__dirname, 'public')));
 
+const getMovies = () => {
+    const data = fs.readFileSync
+    (path.join(__dirname, 'src/data/movies.json'));
+    return JSON.parse(data);
+};
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Forðsíða'});
+    const movies = getMovies();
+    res.render('index', { title: 'Kill for Humanity', movies });
 });
 
-app.get('/about', (req, res) => {
-    res.render('about', { title: 'Um okkur'});
-});
+app.get('/movie/:id', (req, res) => {
+    const movies = getMovies();
+    const movie = movies.find(m => m.id === req.params.id);
 
-app.use((req, res) =>{
-    res.status(404).send('Síða fannst ekki (404)');
-});
+    if (!movie) {
+        return res.status(404).render('404', {  title: 'Síða fannst ekki'});
+    }
+
+    res.render('movie-details', { title: movie.title, movie });
+})
 
 app.listen(PORT, () => {
     console.log(`Server keyrir á http://localhost:${PORT}`);
